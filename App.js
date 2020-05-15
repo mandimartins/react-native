c79c68fba122ab9765c0090dc9ff9ca28b738246
mandimartins <mandimartins23@gmail.com>
2020-05-15 20:05:28 -0300

    Add inline styles

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('Text form state');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <TextInput
+          placeholder="Username"
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 2,
+            padding: 5,
+            width: '80%',
+          }}
+        />
+        <Button title="App" />
+      </View>
     </View>
   );
 }
